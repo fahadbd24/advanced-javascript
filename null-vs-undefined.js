d6420e6ab9cbba4fdb31697bb7b4f3ea549c7e8c
fahadbd24 <fahadbd24@gmail.com>
2020-09-01 13:21:31 +0600

    done

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+let pakhi;
+
+function add (num1, num2) {
+    console.log(num1 +num2);
+    return
+    
+}
+
+function add(num1,num2=10) {
+    console.log(num2);
+    
+}
+
+const result1 = add (12);
+console.log(result1);
+
+const result = add (12,13);
+console.log(result);
+
+const premik = {name:"Salman Khan", phone:738383, address: "Dhaka"};
+console.log(premik.address);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2,3,5];
+console.log(ages [11]);
+
