60e874924996d7e335fb21273796af55ad163a03
fahadbd24 <fahadbd24@gmail.com>
2020-09-01 11:32:47 +0600

    true vs fale

---
+++ true-false.js
@@ -0,0 +1,12 @@
+// const name = " ";
+
+let name = NaN;
+console.log(name);
+
+if (name.length > 0) {
+    console.log("conidition is true")
+    
+} else {
+    console.log("condition is false")
+    
+}
