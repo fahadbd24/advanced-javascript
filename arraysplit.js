1d3a33b5405daf02b87d78ba2820aab9b2b50c5d
fahadbd24 <fahadbd24@gmail.com>
2020-09-02 13:34:55 +0600

    module done

---
+++ arraysplit.js
@@ -0,0 +1,12 @@
+const numbers = [ 1,2,3,4,5,6,7];
+// const part = numbers.slice (2,5);
+// console.log(part);
+// console.log(numbers);
+
+// const removed = numbers.splice(2,5, 77,99);
+// console.log(removed);
+// console.log(numbers);
+
+const together = numbers.join("ami fahad ");
+console.log(together);
+
