988668fed393689f29042bcbac552e973c6a1660
fahadbd24 <fahadbd24@gmail.com>
2020-09-02 12:31:23 +0600

    understand closure

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus = 20; 
+
+function  sum(first, second) {
+    const result = first + second + bonus;
+    if ( result > 9) {
+        var  mood = "happy";
+        mood = "sad";
+        mood = "angry";
+        mood = "funny";
+        console.log(mood);
+        
+    }
+    return result;   
+}
+// const output =sum (5,3);
+// console.log(bonus);
+// console.log(output);
+
+console.log(mood);
